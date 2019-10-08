8699aa81942f334d1a2718bb2fe6b58b0c6cb6be
Dmitry Chueshov <dimka-chueshov@mail.ru>
2019-10-09 01:59:27 +0300

    add thread in dbhelper

---
+++ src/main/java/DBhelper.java
@@ -0,0 +1,44 @@
+import java.sql.*;
+
+
+public class DBhelper extends Thread {
+    private static final String url = "jdbc:mysql://localhost:3306/world?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String username = "root";
+    private static final String passwordname = "root";
+    private Connection cn = null;
+    private Statement statement=null;
+
+
+    public DBhelper() {
+
+    }
+
+    public void run(){
+
+        try {
+            cn=DriverManager.getConnection(url,username,passwordname);
+            statement=cn.createStatement();
+
+            //statement.execute("INSERT INTO world.humans(name, surname) VALUES ('Ludmila','Truhanovich');");//вставка
+
+           // statement.executeUpdate("Update `humans` set surname='Pety' where id=16;");//апдейт данных в таблице
+//
+//            statement.execute("SELECT*FROM world.humans VALUE WHERE surname='Chueshov'");
+
+
+            ResultSet rs=statement.executeQuery("SELECT*FROM humans"); //Executequery позволяет делать только селекты через резол сет
+    //        ArrayList<String>list=new ArrayList<String>();disabled, arraylist add in result set for receive values out database
+
+            while (rs.next()){
+               System.out.println(rs.getString("surname")); //получение данных с таблицы по колонке surname
+                }
+
+            cn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, InterruptedException {
+
+        DBhelper dBhelper=new DBhelper();
+        dBhelper.start();
+
+
+
+
+
+    }
+}
