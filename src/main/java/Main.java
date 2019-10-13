90b64a379283baedd38cbdea6fc971262a1f8f57
Dmitry Chueshov <dimka-chueshov@mail.ru>
2019-10-13 14:25:31 +0300

    add DB helper class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,25 @@
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException, InterruptedException {
+//
+//        try {
+//            FileWriter fileWriter=new FileWriter("C://c.txt");
+//            for (int i=0;i<=10;i++){
+//                fileWriter.write(Integer.toString(i));
+//            }
+//
+//            fileWriter.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
-        DBhelper dBhelper=new DBhelper();
-        dBhelper.start();
+
+//        DBhelper dBhelper=new DBhelper();
+//        dBhelper.start();
 
 
 
